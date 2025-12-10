29b6e046e73d2b3783f8382291d870656e783c56
Tafsirchy <tafsirchy1000@gmail.com>
2025-12-10 20:39:59 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p>Hello Good People</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            From Home
+        </div>
+    );
+};
+
+export default Home;
